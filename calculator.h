3b38ba780de675e1bc327e190fa2c175ad6618d9
Melove Gupta <melovegupta0406@gmail.com>
2026-03-13 00:46:19 +0530

    Refactor comments in calculator.h for clarity

--- calculator.h
+++ calculator.h
@@ -1,6 +1,5 @@
 // calculator.h
 // this is my calculator header file... has all the function declarations
-// i think thats what headers are for lol
 
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
@@ -10,11 +9,11 @@ int add(int a, int b);
 int subtract(int a, int b);
 int multiply(int a, int b);
 
-// divison function... handles divide by zero
+// divison function(handles divide by zero)
 float divide(float a, float b);
 
-// extra functions i added later
+// extra functions
 int absolute(int num);
-int power(int base, int exp);  // not sure if exp is a good name but whatever
+int power(int base, int exp);
 
 #endif
